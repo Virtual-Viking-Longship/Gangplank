5bfcbc5feb9df4186325665fcd6c12b65c673dc2
OBJ2026 <156019006+OBJ2026@users.noreply.github.com>
2025-07-22 11:49:16 -0500

    Revert "fixing some bugs"
    This reverts commit f2e6c433d9b038b5ff4df15cc55008c2cfa75e4d.

--- Assets/HandMenuChecker.cs
+++ Assets/HandMenuChecker.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class HandMenuChecker : MonoBehaviour
 {
-    public OVRHand Hand;
-    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Hand.IsSystemGestureInProgress)
-        {
-            GameObject Menu = GameObject.FindWithTag("Menu");
-            Menu.active = true;
-
-        }
+        
     }
 }
