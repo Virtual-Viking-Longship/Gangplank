258e1d7c8d0dcbf489a55aef7b6ded542b9491ed
OBJ2026 <156019006+OBJ2026@users.noreply.github.com>
2025-07-23 12:29:13 -0500

    menupositioning
    changed menu positioning and removed directional light.

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -22,7 +22,7 @@ public class MenuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
+        menu.transform.LookAt(new Vector3(head.position.x, head.position.y, head.position.z));
         menu.transform.forward *= -1;
     }
 }
