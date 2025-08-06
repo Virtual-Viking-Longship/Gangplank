48d6fe33d2933c427edb2a93e59c505b0a788adf
OBJ2026 <156019006+OBJ2026@users.noreply.github.com>
2025-08-06 13:23:01 -0500

    Added Ship Building Scene
    - pulled OVR rig into new Core-Scene
    - Configured scenemanagerandfader
    - fixed a lot of shader things (more to do)
    -Ship Building is now the first scene to pop up

--- Assets/SceneFader.cs
+++ Assets/SceneFader.cs
@@ -5,10 +5,17 @@ public class SceneFader : MonoBehaviour
 {
     public Animator animator;
     private int destination;
+    private Scene world;
+    private Scene build;
+    private Scene ship;
+    private Scene row;
 
-    public string[] sceneNames;
     void Start()
     {
+        world = SceneManager.GetSceneByName("World-Environment");
+        build = SceneManager.GetSceneByName("1 to 10 Ship Building");
+        ship = SceneManager.GetSceneByName("Ship-With-Annotations");
+        row = SceneManager.GetSceneByName("Rowing-Game");
         destination = 0;
         FadeComplete();
     }
@@ -21,30 +28,45 @@ public class SceneFader : MonoBehaviour
 
     public void FadeComplete ()
     {
+        world = SceneManager.GetSceneByName("World-Environment");
+        build = SceneManager.GetSceneByName("1 to 10 Ship Building");
+        ship = SceneManager.GetSceneByName("Ship-With-Annotations");
+        row = SceneManager.GetSceneByName("Rowing-Game");
         GameObject player = GameObject.FindWithTag("Player");
-        //GameObject Camera = GameObject.FindWithTag("Main Camera");
+        if (!world.isLoaded)
+            {
+                SceneManager.LoadScene("World-Environment", LoadSceneMode.Additive);
+            }
         if (destination == 0)  // Shoreline
         {
-            player.transform.SetPositionAndRotation(new Vector3(-24, -0.187f, 3), new Quaternion(0, 0, 0, 0));
-            //Camera.transform.rotation = new Quaternion(0, 90, 0, 0);
-            for (int i = 0; i < sceneNames.Length; i++)
+            if (!build.isLoaded)
             {
-                SceneManager.LoadScene(sceneNames[i], LoadSceneMode.Additive);
+                SceneManager.LoadScene("1 to 10 Ship Building", LoadSceneMode.Additive);
             }
         }
         if (destination == 1) //Back of Ship Annotations
         {
-            player.transform.SetPositionAndRotation(new Vector3(-15, 0, 1), new Quaternion(0, 0, 0, 180));
+            if (!ship.isLoaded)
+            {
+                SceneManager.LoadScene("Ship-With-Annotations", LoadSceneMode.Additive);
+            }
         }
         if (destination == 2) // Front of Ship Annotations
         {
+            if (!ship.isLoaded)
+            {
+                SceneManager.LoadScene("Ship-With-Annotations", LoadSceneMode.Additive);
+            }
             player.transform.SetPositionAndRotation(new Vector3(-15, 0, 7), new Quaternion(0, 0, 0, 90));
         }
         if (destination == 3) // Rowing MiniGame
         {
             player.transform.SetPositionAndRotation(new Vector3(-15.83f, -0.187f, 7.071f), new Quaternion(0, 0, 0, 0));
-            //Camera.transform.rotation = new Quaternion(0, 180, 0, 0);
-            for (int i = 0; i < sceneNames.Length; i++)
+            if (!ship.isLoaded)
+            {
+                SceneManager.LoadScene("Ship-With-Annotations", LoadSceneMode.Additive);
+            }
+            if (!row.isLoaded)
             {
                 SceneManager.LoadScene("Rowing-Game", LoadSceneMode.Additive);
             }
